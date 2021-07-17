a51aae852e9e2afa2452522e0166229bc6c94506
masal9pse <returnymgstokh@icloud.com>
2021-07-17 13:23:33 +0900

    provider 写経

--- lib/main.dart
+++ lib/main.dart
@@ -1,124 +1,169 @@
+// @dart=2.9
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_provider/result_provider.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
+  runApp(
+    MultiProvider(
       providers: [
         ChangeNotifierProvider<ResultProvider>(
           create: (context) => ResultProvider(),
         ),
       ],
       child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-        ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        home: MyHomePage(),
       ),
+    ),
+  );
+}
+
+class ResultProvider extends ChangeNotifier {
+  String _result;
+
+  ResultProvider() {
+    initValue();
+  }
+
+  // 初期化
+  void initValue() {
+    this._result = "遷移先に移動です";
+  }
+
+  void refresh() {
+    initValue();
+    notifyListeners(); // Providerを介してConsumer配下のWidgetがリビルドされる
+  }
+
+  void updateText(String str) {
+    _result = str;
+  }
+
+  void notify() {
+    notifyListeners(); // Providerを介してConsumer配下のWidgetがリビルドされる
+  }
+}
+
+// 遷移元ページ
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // テキスト表示
+    Widget _renderText(ResultProvider model) {
+      // print('text:${model._result}');
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              model._result, // ResultProviderのプロパティ
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            RaisedButton(
+              child: Text('Go to Edit Pageです'),
+              onPressed: () async {
+                model.updateText('Hello! from HomePage.');
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                    // 引数に遷移元から遷移先へ渡す値を設定
+                    EditPage(),
+                  ),
+                );
+                // print(result);
+                model.notify(); // ResultProviderのメソッド
+              },
+            ),
+          ],
+        ),
+      );
+    }
+
+    // サンプル１：Scaffold全体をリビルド
+    return Consumer<ResultProvider>(builder: (context, model, _) {
+      return Scaffold(
+        appBar: appBar(),
+        body: _renderText(model),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            model.refresh();
+          },
+          child: Icon(
+            Icons.refresh,
+          ),
+        ),
+      );
+    });
+
+    // サンプル２：appBar以外をリビルド
+    // return Scaffold(
+    //   appBar: appBar(),
+    //   body: Consumer<ResultProvider>(builder: (context, model, _) {
+    //     return _renderText(model);
+    //   }),
+    //   floatingActionButton:
+    //   Consumer<ResultProvider>(builder: (context, model, _) {
+    //     return FloatingActionButton(
+    //       onPressed: () {
+    //         model.refresh(); // ResultProviderのメソッド
+    //       },
+    //       child: Icon(
+    //         Icons.refresh,
+    //       ),
+    //     );
+    //   }),
+    // );
+  }
+
+  appBar() {
+    print('appBar実行');
+    return AppBar(
+      title: Text('My Home Page（遷移元）'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+// 遷移先ページ
+class EditPage extends StatelessWidget {
+  final receive;
+  const EditPage({Key key, this.receive}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Consumer<ResultProvider>(
+      builder: (context, model, child) {
+        return WillPopScope(
+          onWillPop: () {
+            model.updateText('Thank you! from 戻るアイコン'); // ResultProviderのメソッド
+            Navigator.pop(context);
+            return Future.value(false);
+          },
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text('Edit Page（遷移先）'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    model._result,
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  RaisedButton(
+                      child: Text('Return'),
+                      onPressed: () {
+                        model.updateText(
+                            'Thank you! from 戻るボタン'); // ResultProviderのメソッド
+                        Navigator.pop(context);
+                      }),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+        );
+      },
     );
   }
 }
